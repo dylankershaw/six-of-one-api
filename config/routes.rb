2af8b5943d8f68744f66e8b7571c824253eca1e2
github username <dylankershaw@me.com>
2018-01-27 16:48:59 -0500

    seeded posts and created routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api do
     namespace :v1 do
-      resources :posts, only: [:show, :create]
+      resources :posts, param: :slug, only: [:create, :show]
+      # get '/posts/:slug', to: "posts#show"
     end
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Post.create([
+    { 
+        title: "It is impossible to walk rapidly and be unhappy.", 
+        slug: "it-is-impossible-to-walk-rapidly-and-be-unhappy" 
+    }, 
+    { 
+        title: "We don't get offered crises, they arrive.", 
+        slug: "we-dont-get-offered-crises-they-arrive "
+    }, 
+    { 
+        title: "I have seen the future and it doesn't work.", 
+        slug: "i-have-seen-the-future-and-it-doesnt-work" 
+    }, 
+    { 
+        title: "I dwell in possibility...", 
+        slug: "i-dwell-in-possibility" 
+    }, 
+    { 
+        title: "Knowledge is power.", 
+        slug: "knowledge-is-power" 
+    }
+])
