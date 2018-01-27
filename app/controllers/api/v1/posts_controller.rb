a0c69c35399a719f300e818d8c6a3b9a2dabf3ba
github username <dylankershaw@me.com>
2018-01-27 13:36:04 -0500

    add post create and show controllers

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,2 +1,15 @@
 class Api::V1::PostsController < ApplicationController
+    def create
+        slug = params["title"].downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
+        
+        # if slug exists, add random characters to the end of it
+
+        post = Post.new(title: params["title"], slug: slug)
+        render json: post
+    end
+
+    def show
+        post = Post.find_by(slug: params[:slug])
+        render json: post
+    end
 end
