3f962c16e799ad5b41fd5422ca8a2cd5678a5bc3
github username <dylankershaw@me.com>
2018-01-28 11:16:41 -0500

    remove slug creation from posts controller

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,15 +1,11 @@
 class Api::V1::PostsController < ApplicationController
-    def create
-        slug = params["title"].downcase.strip.gsub(' ', '-').gsub(/[^\w-]/, '')
-        
-        # if slug exists, add random characters to the end of it
-
-        post = Post.new(title: params["title"], slug: slug)
+    def create        
+        post = Post.new(title: params["title"], slug: params["slug"])
         render json: post
     end
 
     def show
-        post = Post.find_by(slug: params[:slug])
+        post = Post.find_by(slug: params["slug"])
         render json: post
     end
 end
